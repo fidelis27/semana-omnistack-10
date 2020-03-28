1cf173b1f65117afa838a046070bb2cbab5d1e97
fidelis27 <thiago.fidelis27@gmail.com>
2020-03-28 15:19:31 -0300

    add backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+    'mongodb+srv://thiago:thiago27@omnistack-aegif.mongodb.net/semana10?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
